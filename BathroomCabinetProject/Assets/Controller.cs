932076d9eba92b137375ae294434b905eaf6b414
peachpieproductions <benjames.im@gmail.com>
2018-06-11 19:34:20 -0400

    Fixes, cam movement

--- BathroomCabinetProject/Assets/Controller.cs
+++ BathroomCabinetProject/Assets/Controller.cs
@@ -5,75 +5,83 @@ using UnityEngine.UI;
 
 public class Controller : MonoBehaviour {
 
-    public Transform set1;
-    public Transform set2;
     public bool altCounterTop;
     public bool altFaucet;
     public bool altSideCab;
     public bool altSinkCab;
     public bool altUpperCab;
 
+    public Transform camOrigin;
+    public List<GameObject> set1;
+    public List<GameObject> set2;
     public List<Toggle> toggles = new List<Toggle>();
 
-    private void Start() {
-        foreach (Toggle t in FindObjectsOfType<Toggle>()) toggles.Add(t);
-    }
-
     private void Update() {
-        altCounterTop = toggles[4].isOn;
-        altFaucet = toggles[3].isOn;
+
+        //inputs
+        if (Input.GetMouseButton(0)) {
+            camOrigin.Rotate(-Input.GetAxis("Mouse Y") * 1.5f, -Input.GetAxis("Mouse X") * 1.5f, 0,Space.World);
+            camOrigin.eulerAngles = new Vector3(camOrigin.eulerAngles.x, camOrigin.eulerAngles.y, 0);
+        }
+        if (Input.GetMouseButton(1)) {
+            Camera.main.fieldOfView += -Input.GetAxis("Mouse Y");
+        }
+
+        //get toggles
+        altCounterTop = toggles[0].isOn;
+        altFaucet = toggles[1].isOn;
         altSideCab = toggles[2].isOn;
-        altSinkCab = toggles[1].isOn;
-        altUpperCab = toggles[0].isOn;
+        altSinkCab = toggles[3].isOn;
+        altUpperCab = toggles[4].isOn;
 
         //Counter Top
         if (altCounterTop) {
-            set1.GetChild(0).gameObject.SetActive(false);
-            set2.GetChild(0).gameObject.SetActive(true);
+            set1[0].gameObject.SetActive(false);
+            set2[0].gameObject.SetActive(true);
         }
         else {
-            set1.GetChild(0).gameObject.SetActive(true);
-            set2.GetChild(0).gameObject.SetActive(false);
+            set1[0].gameObject.SetActive(true);
+            set2[0].gameObject.SetActive(false);
         }
 
         //Faucet
         if (altFaucet) {
-            set1.GetChild(1).gameObject.SetActive(false);
-            set2.GetChild(1).gameObject.SetActive(true);
+            set1[1].gameObject.SetActive(false);
+            set2[1].gameObject.SetActive(true);
         }
         else {
-            set1.GetChild(1).gameObject.SetActive(true);
-            set2.GetChild(1).gameObject.SetActive(false);
+            set1[1].gameObject.SetActive(true);
+            set2[1].gameObject.SetActive(false);
         }
 
         //Side Cab
         if (altSideCab) {
-            set1.GetChild(2).gameObject.SetActive(false);
-            set2.GetChild(2).gameObject.SetActive(true);
+            set1[2].gameObject.SetActive(false);
+            set2[2].gameObject.SetActive(true);
         }
         else {
-            set1.GetChild(2).gameObject.SetActive(true);
-            set2.GetChild(2).gameObject.SetActive(false);
+            set1[2].gameObject.SetActive(true);
+            set2[2].gameObject.SetActive(false);
         }
 
         //Sink Cab
         if (altSinkCab) {
-            set1.GetChild(3).gameObject.SetActive(false);
-            set2.GetChild(3).gameObject.SetActive(true);
+            set1[3].gameObject.SetActive(false);
+            set2[3].gameObject.SetActive(true);
         }
         else {
-            set1.GetChild(3).gameObject.SetActive(true);
-            set2.GetChild(3).gameObject.SetActive(false);
+            set1[3].gameObject.SetActive(true);
+            set2[3].gameObject.SetActive(false);
         }
 
         //Upper Cab
         if (altUpperCab) {
-            set1.GetChild(4).gameObject.SetActive(false);
-            set2.GetChild(4).gameObject.SetActive(true);
+            set1[4].gameObject.SetActive(false);
+            set2[4].gameObject.SetActive(true);
         }
         else {
-            set1.GetChild(4).gameObject.SetActive(true);
-            set2.GetChild(4).gameObject.SetActive(false);
+            set1[4].gameObject.SetActive(true);
+            set2[4].gameObject.SetActive(false);
         }
     }
 
