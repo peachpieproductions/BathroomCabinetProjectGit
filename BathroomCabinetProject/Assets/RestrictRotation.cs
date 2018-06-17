1a168e10b3864d8ac3f370d05882aff90c388d77
peachpieproductions <benjames.im@gmail.com>
2018-06-16 20:45:34 -0400

    restrict cam rotation

---
+++ BathroomCabinetProject/Assets/RestrictRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestrictRotation : MonoBehaviour {
+
+
+
+    private void Update() {
+        var rot = transform.eulerAngles;
+        if (rot.x > 180) rot.x -= 360;
+        if (rot.y > 180) rot.y -= 360;
+        if (rot.x < -55) rot.x = 55;
+        if (rot.x > 20) rot.x = 20;
+        if (rot.y > 90) rot.y = 90;
+        if (rot.y < -90) rot.y = -90;
+        transform.eulerAngles = rot;
+    }
+
+
+
+}
